91377d6995297b3231a28c9c3c56683e45c8bc38
ari <ari.grr@gmail.com>
2013-05-14 09:38:55 +0200

    Update data fixed. Show ok/error messages.

--- js/updateData.js
+++ js/updateData.js
@@ -1,17 +1,24 @@
 function updateData() {
-   alert("yay");
-        $('#updating_gif').show();
-        $.ajax({
-            url: 'http://localhost:8082/downloader2/services/authors/',
-            type: 'GET',
-            success: function() {
-                $('#updating_gif').hide();
-                
-            },
-            error: function(data) {
-                alert('woops!'); //or whatever
+    $('#updating_gif').show();
+
+    $.ajax({
+        url: '../html/updateData.php',
+        type: 'GET',
+        success: function(data) {
+            $('#updating_gif').hide();
+            if (data) {
+                $('#updated').show();
+            } else {
+                $('#updated_not').show();
             }
-        });
-    
+
+        },
+        error: function() {
+            $('#updating_gif').hide();
+            $('#not_updated').show();
+        }
+    });
+
+
 }
 
