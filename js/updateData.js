51942f7ea2cc1e9705c341d42f89950d588632e0
ari <ari.grr@gmail.com>
2013-05-17 11:59:26 +0200

    Fix update data messages

--- js/updateData.js
+++ js/updateData.js
@@ -8,14 +8,17 @@ function updateData() {
             $('#updating_gif').hide();
             if (data) {
                 $('#updated').show();
+                $('#not_updated').hide();
             } else {
                 $('#updated_not').show();
+                $('#updated').hide();
             }
 
         },
         error: function() {
             $('#updating_gif').hide();
             $('#not_updated').show();
+            $('#updated').hide();
         }
     });
 
